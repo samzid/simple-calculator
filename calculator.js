3b42254c786e2d219cee7afd6d0a1e73db02c3f3
samjid khan <mdsamjidkhan8@gmail.com>
2022-09-09 20:40:39 +0600

    simple-calculator

---
+++ calculator.js
@@ -0,0 +1,18 @@
+</metadata>
+<font-face font-family="FontAwesome" font-weight="400" font-stretch="normal" units-per-em="1792" panose-1="0 0 0 0 0 0 0 0 0 0" ascent="1536" descent="-256" bbox="-1.02083 -256.962 2304.6 1537.02" underline-thickness="0" underline-position="0" unicode-range="U+0020-F500"/>
+<missing-glyph horiz-adv-x="896" d="M224 112h448v1312h-448v-1312zM112 0v1536h672v-1536h-672z"/>
+<glyph glyph-name=".notdef" horiz-adv-x="896" d="M224 112h448v1312h-448v-1312zM112 0v1536h672v-1536h-672z"/>
+<glyph glyph-name=".null" horiz-adv-x="0"/>
+<glyph glyph-name="nonmarkingreturn" horiz-adv-x="597"/>
+<glyph glyph-name="space" unicode=" " horiz-adv-x="448"/>
+<glyph glyph-name="dieresis" unicode="¨" horiz-adv-x="1792"/>
+<glyph glyph-name="copyright" unicode="©" horiz-adv-x="1792"/>
+<glyph glyph-name="registered" unicode="®" horiz-adv-x="1792"/>
+<glyph glyph-name="acute" unicode="´" horiz-adv-x="1792"/>
+<glyph glyph-name="AE" unicode="Æ" horiz-adv-x="1792"/>
+<glyph glyph-name="Oslash" unicode="Ø" horiz-adv-x="1792"/>
+<glyph glyph-name="trademark" unicode="™" horiz-adv-x="1792"/>
+<glyph glyph-name="infinity" unicode="∞" horiz-adv-x="1792"/>
+<glyph glyph-name="notequal" unicode="≠" horiz-adv-x="1792"/>
+<glyph glyph-name="glass" unicode="" horiz-adv-x="1792" d="M1699 1350q0 -35 -43 -78l-632 -632v-768h320q26 0 45 -19t19 -45t-19 -45t-45 -19h-896q-26 0 -45 19t-19 45t19 45t45 19h320v768l-632 632q-43 43 -43 78q0 23 18 36.5t38 17.5t43 4h1408q23 0 43 -4t38 -17.5t18 -36.5z"/>
+<glyph glyph-name="music" unicode="" d="M1536 1312v-1120q0 -50 -34 -89t-86 -60.5t-103.5 -32t-96.5 -10.5t-96.5 10.5t-103.5 32t-86 60.5t-34 89t34 89t86 60.5t103.5 32t96.5 10.5q105 0 192 -39v537l-768 -237v-709q0 -50 -34 -89t-86 -60.5t-103.5 -32t-96.5 -10.5t-96.5 10.5t-103.5 32t-86 60.5t-34 89 t34 89t86 60.5t103.5 32t96.5 10.5q105 0 192 -39v967q0 31 19 56.5t49 35.5l832 256q12 4 28 4q40 0 68 -28t28 -68z"/>
